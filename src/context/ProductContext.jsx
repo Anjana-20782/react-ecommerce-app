3991ab87e47a4a28469c70b32599c96129c83ed8
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-04 16:12:52 +0530

    new contents added in e-commerce

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const ProductContext = createContext();
+
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get("https://dummyjson.com/products");
+        setProducts(data.products);
+      } catch (err) {
+        console.error("Error loading products", err);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
