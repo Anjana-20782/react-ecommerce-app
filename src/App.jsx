bfdc1230f2ba579b16b725e48fe95d9bd3739b2a
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-04 16:23:38 +0530

    new contents added in e-commerce

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ProductProvider } from "./context/ProductContext";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
+import { ProductProvider } from "./context/ProductContext.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Products from "./pages/ Products.jsx";
 
 export default function App() {
   return (
